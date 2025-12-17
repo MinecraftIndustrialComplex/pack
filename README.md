effc2e94882206f7bae4223c530ac855a6eccd91
Benjamin McIntyre <benjaminmcintyre06@protonmail.com>
2025-12-17 18:30:32 -0500

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Minecraft Industrial Complex
+Advanced create based modpack which has an extremely difficult early game, with a create based progression system that results in modern industrial tooling (guns, tanks, etc.) used to conquer this difficult world.
---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Minecraft Industrial Complex dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = [ nixpkgs.legacyPackages.x86_64-linux.packwiz ];
+    };
+  };
+}
