cd8468a9dcef9cd002cf46dcf5bcc901c556c47c
Benjamin McIntyre <benjaminmcintyre06@protonmail.com>
2026-05-28 10:03:54 -0400

    Remove steel based soft-lock

--- kubejs/server_scripts/electron_tube.js
+++ kubejs/server_scripts/electron_tube.js
@@ -2,4 +2,9 @@ ServerEvents.recipes(event => {
   event.remove({ output: 'create:electron_tube' })
   event.shapeless('create:electron_tube', ['create:polished_rose_quartz', '#c:plates/steel'])
   event.recipes.create.deploying('create:electron_tube', [Ingredient.of('#c:plates/steel'), 'create:polished_rose_quartz'])
+
+// Prevent softlock from not being able to obtain powdered obsidian
+  event.recipes.create.milling([
+    CreateItem.of('create:powdered_obsidian', 0.5)
+  ], 'minecraft:obsidian')
 })
